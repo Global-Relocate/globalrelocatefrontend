bbecbebe2009c459cef7be17e6ef2e0a3f638edc
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2025-01-03 01:20:16 +0000

    feat: add routing for Login and Signup pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Landing from "./pages/unauthenticated/Landing";
+import Login from "./pages/unauthenticated/Login";
+import Signup from "./pages/unauthenticated/Signup";
+import SignupForm from "./pages/unauthenticated/SignupForm";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signupform" element={<SignupForm />} />
     </Routes>
   );
 }
