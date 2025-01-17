815af9954e8965fe8f89babb78b32ee1fcaf2199
jossyboydgenius <jossyboydgenius@gmail.com>
2025-01-17 15:22:32 +0000

    Add OAuthCallback component to handle OAuth authentication callback

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ForgotPassword from "./pages/unauthenticated/ForgotPassword";
 import VerifyEmail from "./pages/unauthenticated/VerifyEmail";
 import ResetPassword from "./pages/unauthenticated/ResetPassword";
 import Welcome from "./pages/authenticated/Welcome";
+import OAuthCallback from "./pages/unauthenticated/OAuthCallback";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Route path="/verifymail" element={<VerifyEmail />} />
       <Route path="/resetpassword" element={<ResetPassword />} />
       <Route path="/welcome" element={<Welcome />} />
+      <Route path="/oauth/callback" element={<OAuthCallback />} />
     </Routes>
   );
 }
