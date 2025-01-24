220fd86a6d8bfb1e006fd83008bca5e938ca9e23
jossyboydgenius <jossyboydgenius@gmail.com>
2025-01-24 21:14:28 +0000

    Refactor DashNav component to simplify dropdown menu structure
    - Removed unused dropdown menu components and streamlined the dropdown menu implementation within the DashNav component.
    - Updated the user profile trigger to enhance usability and maintain a cleaner codebase.
    - This refactor aims to improve the performance and maintainability of the navigation component while enhancing the user experience.

--- src/components/navigation/DashNav.jsx
+++ src/components/navigation/DashNav.jsx
@@ -3,20 +3,6 @@ import { IoMdClose } from "react-icons/io";
 import SelectLanguages from "../drawers/SelectLanguages";
 import { LuUserRound } from "react-icons/lu";
 import logo from "../../assets/svg/logo.svg";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { IoChevronDownOutline } from "react-icons/io5";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
@@ -62,55 +48,6 @@ function DashNav({ navState, setNavState }) {
 
       <div className="flex items-center space-x-3">
         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <div className="flex items-center justify-start space-x-1 p-2 rounded-3xl border">
-              <div className="flex text-white items-center justify-center h-7 w-7 rounded-full bg-gray-500">
-                <FaUser />
-              </div>
-              <span className="text-xs">Jerry Lamp</span>
-            </div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                Profile
-                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-              </DropdownMenuItem>
-
-              <DropdownMenuItem>
-                Settings
-                <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuSub>
-                <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-                <DropdownMenuPortal>
-                  <DropdownMenuSubContent>
-                    <DropdownMenuItem>Email</DropdownMenuItem>
-                    <DropdownMenuItem>Message</DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>More...</DropdownMenuItem>
-                  </DropdownMenuSubContent>
-                </DropdownMenuPortal>
-              </DropdownMenuSub>
-              <DropdownMenuItem>
-                New Team
-                <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>GitHub</DropdownMenuItem>
-            <DropdownMenuItem>Support</DropdownMenuItem>
-            <DropdownMenuItem disabled>API</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              Log out
-              <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
           <DropdownMenuTrigger className="flex items-center justify-start space-x-1 p-2 rounded-3xl cursor-pointer hover:bg-gray-100 outline-none">
             <div className="flex text-white items-center justify-center h-7 w-7 rounded-full bg-gray-500">
               <LuUserRound />
