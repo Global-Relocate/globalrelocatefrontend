5bfa558336777c7c447fa30c9ca93a5c03c0b0e0
jossyboydgenius <jossyboydgenius@gmail.com>
2025-01-29 17:02:02 +0000

    refactor: remove Settings route and import

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import Favourites from "./pages/user/Favourites";
 import Community from "./pages/user/Community";
 import Notifications from "./pages/user/Notifications";
 import CountryDetails from "./pages/user/CountryDetails";
-import Settings from "./pages/user/Settings";
+// import Settings from "./pages/user/Settings";
 import Profile from "./pages/user/Profile";
 import "./App.css";
 
@@ -43,7 +43,7 @@ function App() {
         <Route path="notifications" element={<Notifications />} />
         <Route path="favourites" element={<Favourites />} />
         <Route path="community" element={<Community />} />
-        <Route path="settings" element={<Settings />} />
+        {/* <Route path="settings" element={<Settings />} /> */}
         <Route path="profile" element={<Profile />} />
       </Route>
     </Routes>
