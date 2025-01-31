76676fc7fbe85dd5f30a4cd65cc83f94c7c2b956
jossyboydgenius <jossyboydgenius@gmail.com>
2025-01-31 06:58:09 +0000

    refactor: remove DashboardLayout from Notifications page

--- src/pages/user/Notifications.jsx
+++ src/pages/user/Notifications.jsx
@@ -1,10 +1,9 @@
-import DashboardLayout from "@/components/layouts/DashboardLayout";
 import React from "react";
 import bellicon from "../../assets/svg/bell.svg";
 
 function Notifications() {
   return (
-    <DashboardLayout>
+    <>
       <div className="w-full flex-wrap gap-y-5 items-center justify-between flex">
         <h2 className="text-3xl font-medium">Notifications</h2>
       </div>
@@ -15,9 +14,9 @@ function Notifications() {
           className="mb-4"
           style={{ width: '36px', height: '36px', filter: 'invert(41%) sepia(0%) saturate(0%) hue-rotate(180deg) brightness(91%) contrast(88%)' }}
         />
-        <p className="text-gray-600">You haven’t any notifications yet.</p>
+        <p className="text-gray-600">You haven't any notifications yet.</p>
       </div>
-    </DashboardLayout>
+    </>
   );
 }
 
