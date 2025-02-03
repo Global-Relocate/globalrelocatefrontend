5732d494901260220b21ba3c5cf7d2bf2a11734b
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-03 19:43:49 +0000

    feat: Create MentionsTab component with empty state notification UI

---
+++ src/components/profile/tabs/MentionsTab.jsx
@@ -0,0 +1,17 @@
+import bellicon from "../../../assets/svg/bell.svg";
+
+const MentionsTab = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[45vh]">
+      <img
+        src={bellicon}
+        alt="Bell Icon"
+        className="mb-4"
+        style={{ width: '36px', height: '36px', filter: 'invert(41%) sepia(0%) saturate(0%) hue-rotate(180deg) brightness(91%) contrast(88%)' }}
+      />
+      <p className="text-gray-600">You don&apos;t have any mention notifications yet.</p>
+    </div>
+  );
+};
+
+export default MentionsTab; 
