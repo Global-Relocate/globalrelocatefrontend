038db5487838e9043d66b72acf7782d6f21d7b19
jossyboydgenius <jossyboydgenius@gmail.com>
2025-02-03 19:43:35 +0000

    feat: Create AllNotificationsTab component with empty state UI

---
+++ src/components/profile/tabs/AllNotificationsTab.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import bellicon from "../../../assets/svg/bell.svg";
+
+const AllNotificationsTab = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[45vh]">
+      <img
+        src={bellicon}
+        alt="Bell Icon"
+        className="mb-4"
+        style={{ width: '36px', height: '36px', filter: 'invert(41%) sepia(0%) saturate(0%) hue-rotate(180deg) brightness(91%) contrast(88%)' }}
+      />
+      <p className="text-gray-600">You don&apos;t have any notifications yet.</p>
+    </div>
+  );
+};
+
+export default AllNotificationsTab; 
