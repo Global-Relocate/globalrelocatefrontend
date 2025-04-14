2895071325a7d894aa938662204c07d2d2ecac2b
jossyboydgenius <jossyboydgenius@gmail.com>
2025-04-14 14:55:32 +0000

    refactor: simplify Footer component by removing unused animation variants and enhancing layout responsiveness

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import logo from "../../assets/images/footer_logo.png";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
@@ -20,55 +20,9 @@ export default function Footer() {
     }
   };
 
-  // Animation variants
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.1,
-        delayChildren: 0.3
-      }
-    }
-  };
-
-  const itemVariants = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        type: "spring",
-        stiffness: 100
-      }
-    }
-  };
-
-  const logoVariants = {
-    hover: {
-      scale: 1.05,
-      rotate: 3,
-      transition: {
-        type: "spring",
-        stiffness: 300
-      }
-    }
-  };
-
-  const linkVariants = {
-    hover: {
-      // scale: 1.05,
-      color: "#FCA311", // Highlight color
-      transition: {
-        type: "spring",
-        stiffness: 300
-      }
-    }
-  };
-
   return (
-    <div className="text-[#7E7E7E] w-full flex items-center justify-center flex-col bg-black min-h-[300px]">
-      <div className="w-[90%] flex-wrap flex items-start justify-start gap-14 md:gap-36 p-4">
+    <div className="text-[#7E7E7E] w-full flex items-center justify-center flex-col bg-black min-h-[300px] min-w-[320px]">
+      <div className="w-[90%] flex-wrap flex items-start justify-start gap-8 md:gap-36 p-4">
         <div>
           <img
             src={logo}
@@ -117,7 +71,7 @@ export default function Footer() {
           <p className="text-sm mb-4">
             Stay updated with the latest insights on global relocation
           </p>
-          <form onSubmit={handleSubmit} className="flex gap-2">
+          <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row gap-2">
             <input
               type="email"
               value={email}
