1f1b802d25d670cb5c6dd50b14a2e238e072c481
jossyboydgenius <jossyboydgenius@gmail.com>
2025-04-14 15:01:04 +0000

    refactor: update MainLayout component to prevent horizontal overflow and improve layout consistency

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import Navbar from "../navigation/Navbar";
 import Footer from "../navigation/Footer";
 
+// eslint-disable-next-line react/prop-types
 function MainLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-x-hidden">
       <div className="flex-grow">
         <Navbar />
         {children}
